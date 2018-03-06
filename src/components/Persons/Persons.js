8e5a91d3f21b870f46e8e7d8fce48ae3f23eb7ba
Diego Ballesteros <diballesterosc@gmail.com>
2018-03-06 17:48:40 -0500

    Lifecycle Hooks
    -Implemented rudimentary lifecycle hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -3,7 +3,31 @@ import Person from './Person/Person';
 
 
 class Persons extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Persons.js] Inside Constructor', props);
+    
+      }
+    
+      componentWillMount() {
+        console.log('[Persons.js] Inside componentWillMount()');
+      }
+    
+      componentDidMount() {
+        console.log('[Persons.js] inside componentDidMount()');
+      }
+
+      componentWillReceiveProps(){
+          console.log('[UPDATE Persons.js] Inside componenetWillReceiveProps', nextProps);
+      }
+
+      shouldComponentUpdate(nextProps, nextState){
+        console.log('[UPDATE Persons.js] shouldComponentUpdate', nextProps, nextState);
+      }
+
+
     render () {
+        console.log('[Persons.js] inside render()');
         return this.props.persons.map((person,index) => {
             return <Person
               click={() => this.props.clicked(index)} 
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,11 +5,25 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside Constructor', props);
+
+  }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] inside componentDidMount()');
+  }
+
   state = {
     persons: [
-      {id: '1', name: 'Max', age: 28 },
-      {id: '2', name: 'Manu', age: 29 },
-      {id: '3', name: 'Stephanie', age: 26 }
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other unaffected value',
     showPersons: false
@@ -32,7 +46,7 @@ class App extends Component {
 
     persons[personIndex] = person;
 
-    this.setState( {persons: persons});
+    this.setState({ persons: persons });
   }
 
   deletePersonHandler = (personIndex) => {
@@ -41,7 +55,7 @@ class App extends Component {
     // Slice without elements copies the array
     //const persons = this.state.persons.slice();
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
+    this.setState({ persons: persons });
   }
 
   togglePersonsHandler = () => {
@@ -51,11 +65,13 @@ class App extends Component {
 
   render() {
 
+    console.log('[App.js] inside render()');
+
     let persons = null;
 
     if (this.state.showPersons) {
       persons = (
-          <Persons
+        <Persons
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler} />
@@ -64,9 +80,9 @@ class App extends Component {
     return (
       <div className="App">
         <Cockpit
-        showPersons={this.state.showPersons} 
-        persons={this.state.persons}
-        clicked={this.togglePersonsHandler} />
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     );
